75fa6c5a74c7fd19fe9e72e8fe41c90e5f90d193
Sarunas <ramanauskassaras@gmail.com>
2015-02-09 10:29:36 +0200

    IE support script

---
+++ html/js/ie-form-script.js
@@ -0,0 +1,32 @@
+
+function msieversion() {
+
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf("MSIE ");
+
+        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer, return version number
+            return true;//alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+        else                 // If another browser, return 0
+            return false;//alert('otherbrowser');
+
+   //return false;
+}
+
+$( document ).ready( function() {
+		if ( !msieversion() ){
+		}else{
+			
+			$("#form-wrapper form").submit( function(){
+				
+				alert("IE submit");
+				
+				var inputs = $("#form-wrapper").find("input[type='text']");
+				
+				alert(inputs.size());
+				
+				$( this ).append(inputs);
+				
+				return true;
+			});
+		}
+});
